9d13068a3726a7a81e729b860db564e27763dd83
Maficks <berrrserk2020@mail.ru>
2025-02-04 16:23:19 +0500

    adding header

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,12 @@
 </script>
 
 <template>
-  <div>
-
+  <div id="root">
+    <header className="w-full bg-blue-400 h-[17.5vh]">
+      <div className="relative top-[2.8vh] left-[3vw] bg-blue-500">
+        <span>YOUR IMAGE COLLECTION</span>
+      </div>
+    </header>
   </div>
 </template>
 
