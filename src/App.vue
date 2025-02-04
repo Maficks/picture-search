ebbf65a26619b0e746a4a801b2cbb428c87491e8
Maficks <berrrserk2020@mail.ru>
2025-02-05 01:31:42 +0500

    adding styles

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
 <template>
   <div id="root">
     <header className="w-full bg-blue-400 h-[17.5vh]">
-      <div className="relative top-[2.8vh] left-[3vw] bg-blue-500">
-        <span>YOUR IMAGE COLLECTION</span>
+      <div className="relative top-[2.8vh] left-[3vw] bg-blue-500 w-[24.5vw] h-[22.8vh] flex text-end items-center">
+        <span className="place-self-center">YOUR IMAGE COLLECTION</span>
       </div>
     </header>
   </div>
