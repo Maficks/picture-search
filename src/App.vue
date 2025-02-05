330f40b709a5124ec781e21e5203f85026e9fc6d
Maficks <berrrserk2020@mail.ru>
2025-02-05 15:10:29 +0500

    more styles

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,16 @@
 <template>
   <div id="root">
     <header className="w-full bg-blue-400 h-[17.5vh]">
-      <div className="relative top-[2.8vh] left-[3vw] bg-blue-500 w-[24.5vw] h-[22.8vh] flex text-end items-center">
+      <div className="relative top-[2.8vh] left-[3vw] bg-blue-500 w-[24.5vw] h-[22.8vh] flex text-end items-center z-10">
         <span className="place-self-center">YOUR IMAGE COLLECTION</span>
+      </div>
+      <img
+          className="absolute top-[5.2vh] left-[30.4vw] w-[13.75vw]"
+          alt="logo"
+          src="./assets/gul.jpg"
+      />
+      <div className="bg-blue-600 absolute top-[7vh] right-[3vw] h-[14.5%] w-[50%]">
+
       </div>
     </header>
   </div>
